201af23e9fadba3bcb227fd11a00850fefd87ae6
Carolus <113361900+1Carolus1@users.noreply.github.com>
2026-02-03 00:31:53 +0100

    🌐New Routes for sets & socials

---
+++ app/socials/page.tsx
@@ -0,0 +1,9 @@
+import { Navbar } from "@/components/Navbar";
+
+export default function Socials() {
+  return (
+    <div className="min-h-dvh bg-black text-white">
+      <Navbar/>
+    </div>
+  );
+}
