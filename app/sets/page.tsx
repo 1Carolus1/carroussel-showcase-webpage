455bf201f0996a97bb1678ef74356c6c0a08de51
Carolus <113361900+1Carolus1@users.noreply.github.com>
2026-02-20 00:27:53 +0100

    🐛Fixed up mobile sets display

--- app/sets/page.tsx
+++ app/sets/page.tsx
@@ -1,34 +1,33 @@
-'use client'
+"use client";
 import { TrackCard } from "@/components/TrackCard";
 import { SoundCloudTrackData } from "@/types/api/soundcloudTrackData";
 import { useEffect, useState } from "react";
-import './style.css';
 
-  // soundcloud really doesn't provide a better way to fetch a list of tracks :<
-  // next step: find a way to get the tracks data and store them in a database, update them every 24h
+// soundcloud really doesn't provide a better way to fetch a list of tracks :<
+// next step: find a way to get the tracks data and store them in a database, update them every 24h
 
 export default function Sets() {
   const [loading, setLoading] = useState(true);
   const [sets, setSets] = useState<SoundCloudTrackData[]>([]);
   useEffect(() => {
-      async function load() {
-        try {
-          const res = await fetch('/api/getSets');
-          const data = await res.json();
-          setSets(data);
-        } catch (err) {
-          console.error("Failed to load sets", err);
-        } finally {
-          setLoading(false);
-        }
+    async function load() {
+      try {
+        const res = await fetch("/api/getSets");
+        const data = await res.json();
+        setSets(data);
+      } catch (err) {
+        console.error("Failed to load sets", err);
+      } finally {
+        setLoading(false);
       }
-      load();
-    }, []);
-    if (loading) {
-      return null;
     }
+    load();
+  }, []);
+  if (loading) {
+    return null;
+  }
   return (
-    <div className="track-list">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[clamp(20px,3vw,40px)]">
       {sets.length > 0 && sets.map((sets) => <TrackCard key={sets.permanent_track_url} track={sets} />)}
     </div>
   );
